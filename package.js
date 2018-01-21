9eaf971e7fb7581c3c5805e0b443cf463add0273
Rey <reynaldo.reyna@gmail.com>
2018-01-21 13:30:21 -0800

    patches for CoffeeScript 2

--- package.js
+++ package.js
@@ -1,8 +1,8 @@
 Package.describe({
-  name: 'nimble:restivus',
+  name: 'flean:restivus',
   summary: 'Create authenticated REST APIs in Meteor 0.9+ via HTTP/HTTPS. Setup CRUD endpoints for Collections.',
   version: '0.8.12',
-  git: 'https://github.com/kahmali/meteor-restivus.git'
+  git: 'https://github.com/flean/meteor-restivus.git'
 });
 
 
@@ -12,7 +12,7 @@ Package.onUse(function (api) {
 
   // Meteor dependencies
   api.use('check');
-  api.use('coffeescript');
+  api.use('coffeescript@2.0.3_4');
   api.use('underscore');
   api.use('accounts-password@1.3.3');
   api.use('simple:json-routes@2.1.0');
@@ -31,7 +31,7 @@ Package.onTest(function (api) {
   // Meteor dependencies
   api.use('practicalmeteor:munit');
   api.use('test-helpers');
-  api.use('nimble:restivus');
+  api.use('flean:restivus');
   api.use('http');
   api.use('coffeescript');
   api.use('underscore');
