d732a4c49df787029b7468faacc1e044d29223bd
Rey <reynaldo.reyna@gmail.com>
2021-06-22 09:59:25 -0700

    2.3

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'flean:restivus',
   summary: 'Create authenticated REST APIs in Meteor 0.9+ via HTTP/HTTPS. Setup CRUD endpoints for Collections.',
-  version: '0.8.12',
+  version: '0.8.13',
   git: 'https://github.com/flean/meteor-restivus.git'
 });
 
@@ -14,7 +14,7 @@ Package.onUse(function (api) {
   api.use('check');
   api.use('coffeescript@2.0.3_4');
   api.use('underscore');
-  api.use('accounts-password@1.3.3');
+  api.use('accounts-password@1.3.3 || 2.0.0');
   api.use('simple:json-routes@2.1.0');
 
   api.addFiles('lib/auth.coffee', 'server');
